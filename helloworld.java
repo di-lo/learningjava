f65a0dfd5a5be107e383562ce6069afa72a6c84b
Dilara <60364755+di-lo@users.noreply.github.com>
2023-08-08 15:58:28 +0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,11 @@
+public class learningjava{
 
+  public static void main(String[] args){ // args (arguments) stores arguments that are given
+    sayHi(); // Hello World!
+  }
+
+  static void sayHi(){ // method // void is used for not returning
+    System.out.println("Hello World!");
+  }
+
+}
